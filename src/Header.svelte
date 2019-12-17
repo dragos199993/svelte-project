5fb0ea9d83a96bce15d65a8d0ef875f23ac580d6
Dragos Nedelcu <dragos199993@gmail.com>
2019-12-17 19:17:52 +0200

    Learn props.

---
+++ src/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+  let user = { loggedIn: false };
+  const toggle = () => (user.loggedIn = !user.loggedIn);
+</script>
+
+{#if user.loggedIn}
+  <button on:click={toggle}>Log out</button>
+{/if}
+
+{#if !user.loggedIn}
+  <button on:click={toggle}>Log in</button>
+{/if}
